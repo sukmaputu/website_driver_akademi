5e245a7edb2540efe8cb80915ed7c92ce8927dff
I Putu Sukma Widyantara <68043141+sukmaputu@users.noreply.github.com>
2025-11-23 06:48:01 -0800

    perbaikan

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+// Pastikan NAMA REPOSITORI SANGAT JELAS dan MENGGUNAKAN GARIS MIRING DI DEPAN dan DI BELAKANG
 export default defineConfig({
-  // Ubah base: dari '/nama-repo/' menjadi './' 
-  // './' memastikan semua path asset relatif dari index.html
-  base: './', 
+  base: '/website_driver_akademi/', // Ganti jika nama repo berbeda
   
   plugins: [vue()],
 });
