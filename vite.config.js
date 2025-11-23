d6a3a7d336060e9d44af141d38a48fcb5558f25c
I Putu Sukma Widyantara <68043141+sukmaputu@users.noreply.github.com>
2025-11-23 06:43:17 -0800

    perbaikan mime

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 // vite.config.js
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/website-driver-akademi/', 
+  // Ubah base: dari '/nama-repo/' menjadi './' 
+  // './' memastikan semua path asset relatif dari index.html
+  base: './', 
   
   plugins: [vue()],
 });
